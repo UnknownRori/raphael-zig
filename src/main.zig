60fa49d498eb647badd433d8ad96a07102cf9bf5
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2025-08-20 11:10:55 +0700

    chore: refactor TermFreq and also add TermFreqIndex and it's directory parse (no recursive)

--- src/main.zig
+++ src/main.zig
@@ -3,42 +3,33 @@ const lib = @import("raphael_zig_lib");
 
 pub fn main() !void {
     var gpa: std.heap.GeneralPurposeAllocator(.{}) = .init;
+    defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
-    const file_name = "./data/Procrastination.md";
-    const file = try std.fs.cwd().readFileAlloc(allocator, file_name, 4096);
-    defer allocator.free(file);
+    var tfi = lib.TermFreqIndex.init(allocator);
+    defer tfi.deinit();
 
-    var lexer = lib.Lexer.init(allocator);
-
-    var tf = try lexer.parse(file);
-    defer tf.deinit();
+    try tfi.index("./data/");
 
     var buffer = std.ArrayList(u8).init(allocator);
     defer buffer.deinit();
-    var jw = std.json.writeStream(buffer.writer(), .{ .whitespace = .minified });
-    try jw.beginObject();
 
-    var tf_iter = tf.iterator();
-    try jw.objectField(file_name);
-    try jw.beginObject();
-    while (tf_iter.next()) |e| {
-        try stdout.print("{s} -> {d}\n", .{ e.key_ptr.*, e.value_ptr.* });
-        try jw.objectField(e.key_ptr.*);
-        try jw.write(e.value_ptr.*);
-    }
-    try jw.endObject();
-    try jw.endObject();
+    var jw = std.json.writeStream(buffer.writer(), .{ .whitespace = .indent_1 });
+    defer jw.deinit();
+    try tfi.serializeJson(&jw);
 
     var index_file = try std.fs.cwd().createFile("index.json", .{});
     defer index_file.close();
     try index_file.writeAll(buffer.items);
 
-    // try stdout.print("{s}\n", .{buffer.items});
-    // _ = stdout;
+    _ = stdout;
     try bw.flush();
+
+    // TODO: FIX MEMORY LEAK
+    // const leaks = gpa.detectLeaks();
+    // std.debug.print("has leaks : {}", .{leaks});
 }
--- src/root.zig
+++ src/root.zig
@@ -21,21 +21,126 @@ pub const Lexer = struct {
         };
     }
 
-    pub fn parse(self: Self, contents: []const u8) !std.StringHashMap(u32) {
-        var tf = std.StringHashMap(u32).init(self.allocator);
+    pub fn parse(self: Self, contents: []const u8) std.mem.Allocator.Error!TermFreq {
+        var tf = TermFreq.init(self.allocator);
 
-        var lines_sequence = std.mem.tokenizeSequence(u8, contents, end_line());
+        var lines_sequence = std.mem.tokenizeSequence(u8, contents, "\n");
         while (lines_sequence.next()) |line| {
             var line_sequence = std.mem.tokenizeSequence(u8, line, " ");
             while (line_sequence.next()) |word| {
-                if (tf.contains(word)) {
-                    const a = tf.getEntry(word).?;
+                const trimmed = std.mem.trim(u8, word, "\"");
+
+                if (tf.map.contains(trimmed)) {
+                    const a = tf.map.getEntry(trimmed).?;
                     a.value_ptr.* += @as(u32, 1);
                     continue;
                 }
-                _ = try tf.getOrPutValue(word, 1);
+                _ = try tf.map.getOrPutValue(trimmed, 1);
             }
         }
         return tf;
+    }
+};
+
+pub const TermFreq = struct {
+    map: std.StringHashMap(u32),
+    allocator: std.mem.Allocator,
+    const Self = @This();
+
+    pub fn init(allocator: std.mem.Allocator) Self {
+        return Self{
+            .map = std.StringHashMap(u32).init(allocator),
+            .allocator = allocator,
+        };
+    }
+
+    pub fn print(self: Self) void {
+        var it = self.map.iterator();
+        while (it.next()) |e| {
+            std.debug.print("{s} -> {d}\n", .{ e.key_ptr.*, e.value_ptr.* });
+        }
+    }
+
+    pub fn serializeJson(self: Self, jw: anytype) !void {
+        var tf_iter = self.map.iterator();
+        try jw.beginObject();
+        while (tf_iter.next()) |e| {
+            try jw.objectField(e.key_ptr.*);
+            try jw.write(e.value_ptr.*);
+        }
+        try jw.endObject();
+    }
+
+    pub fn deinit(self: *Self) void {
+        self.map.deinit();
+    }
+};
+
+pub const TermFreqIndex = struct {
+    map: std.StringHashMap(TermFreq),
+    allocator: std.mem.Allocator,
+    const Self = @This();
+
+    pub fn init(allocator: std.mem.Allocator) Self {
+        return Self{
+            .map = std.StringHashMap(TermFreq).init(allocator),
+            .allocator = allocator,
+        };
+    }
+
+    pub fn from(map: std.StringHashMap(TermFreq)) Self {
+        return Self{
+            .map = map,
+            .allocator = map.allocator,
+        };
+    }
+
+    pub fn index(self: *Self, directory: []const u8) !void {
+        var dir = try std.fs.cwd().openDir(directory, .{ .iterate = true });
+        defer dir.close();
+
+        const lexer = Lexer.init(self.allocator);
+
+        var it = dir.iterate();
+        while (try it.next()) |val| {
+            // Skip non md file
+            // TODO : Make it work with .txt too and recursively
+            if (!std.mem.containsAtLeast(u8, val.name, 1, ".md") or val.kind != .file) {
+                continue;
+            }
+
+            // TODO: MEM LEAK
+            const file = try dir.readFileAlloc(self.allocator, val.name, 4096);
+
+            const tf = lexer.parse(file) catch |err| {
+                if (err == std.mem.Allocator.Error.OutOfMemory) {
+                    @panic("[-] Buy more RAM yo, lol\n");
+                }
+                std.debug.print("[-] Caught and error", .{});
+                continue;
+            };
+            std.debug.print("----- {s} -----\n", .{val.name});
+            const name = try self.allocator.dupe(u8, val.name);
+            try self.map.put(name, tf);
+        }
+    }
+
+    pub fn serializeJson(self: Self, jw: anytype) !void {
+        try jw.beginObject();
+        var tfi_iter = self.map.iterator();
+        while (tfi_iter.next()) |e| {
+            try jw.objectField(e.key_ptr.*);
+            try e.value_ptr.*.serializeJson(jw);
+        }
+        try jw.endObject();
+    }
+
+    pub fn deinit(self: *Self) void {
+        var tfi_iter = self.map.iterator();
+        while (tfi_iter.next()) |e| {
+            self.allocator.free(e.key_ptr.*);
+            e.value_ptr.*.deinit();
+        }
+        self.map.deinit();
     }
 };
