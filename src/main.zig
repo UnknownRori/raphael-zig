1b611ea43b81a1d38e4319afab7616b4b216f45c
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2025-08-20 07:29:51 +0700

    add: lexer

--- src/main.zig
+++ src/main.zig
@@ -1,46 +1,44 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
+const std = @import("std");
+const lib = @import("raphael_zig_lib");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var gpa: std.heap.GeneralPurposeAllocator(.{}) = .init;
+    const allocator = gpa.allocator();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    const file_name = "./data/Procrastination.md";
+    const file = try std.fs.cwd().readFileAlloc(allocator, file_name, 4096);
+    defer allocator.free(file);
 
-    try bw.flush(); // Don't forget to flush!
+    var lexer = lib.Lexer.init(allocator);
+
+    var tf = try lexer.parse(file);
+    defer tf.deinit();
+
+    var buffer = std.ArrayList(u8).init(allocator);
+    defer buffer.deinit();
+    var jw = std.json.writeStream(buffer.writer(), .{ .whitespace = .minified });
+    try jw.beginObject();
+
+    var tf_iter = tf.iterator();
+    try jw.objectField(file_name);
+    try jw.beginObject();
+    while (tf_iter.next()) |e| {
+        try stdout.print("{s} -> {d}\n", .{ e.key_ptr.*, e.value_ptr.* });
+        try jw.objectField(e.key_ptr.*);
+        try jw.write(e.value_ptr.*);
+    }
+    try jw.endObject();
+    try jw.endObject();
+
+    var index_file = try std.fs.cwd().createFile("index.json", .{});
+    defer index_file.close();
+    try index_file.writeAll(buffer.items);
+
+    // try stdout.print("{s}\n", .{buffer.items});
+    // _ = stdout;
+    try bw.flush();
 }
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "use other module" {
-    try std.testing.expectEqual(@as(i32, 150), lib.add(100, 50));
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
-}
-
-const std = @import("std");
-
-/// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.
-const lib = @import("raphael_zig_lib");
--- src/root.zig
+++ src/root.zig
@@ -1,13 +1,41 @@
-//! By convention, root.zig is the root source file when making a library. If
-//! you are making an executable, the convention is to delete this file and
-//! start with main.zig instead.
 const std = @import("std");
 const testing = std.testing;
 
-pub export fn add(a: i32, b: i32) i32 {
-    return a + b;
+fn end_line() []const u8 {
+    const os = @import("builtin").os;
+    if (os.tag == .windows) {
+        return "\r\n";
+    } else if (os.tag == .linux) {
+        return "\n";
+    }
+    @panic("Unknown OS");
 }
 
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
-}
+pub const Lexer = struct {
+    allocator: std.mem.Allocator,
+    const Self = @This();
+
+    pub fn init(allocator: std.mem.Allocator) Self {
+        return Self{
+            .allocator = allocator,
+        };
+    }
+
+    pub fn parse(self: Self, contents: []const u8) !std.StringHashMap(u32) {
+        var tf = std.StringHashMap(u32).init(self.allocator);
+
+        var lines_sequence = std.mem.tokenizeSequence(u8, contents, end_line());
+        while (lines_sequence.next()) |line| {
+            var line_sequence = std.mem.tokenizeSequence(u8, line, " ");
+            while (line_sequence.next()) |word| {
+                if (tf.contains(word)) {
+                    const a = tf.getEntry(word).?;
+                    a.value_ptr.* += @as(u32, 1);
+                    continue;
+                }
+                _ = try tf.getOrPutValue(word, 1);
+            }
+        }
+        return tf;
+    }
+};
